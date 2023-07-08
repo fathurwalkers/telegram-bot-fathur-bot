5b73833b72d4207372f232148a7b601090d97b2d
Fathur Walkers <fathurwalkers44@gmail.com>
2023-07-08 02:05:13 -0700

    update

--- bot.py
+++ bot.py
@@ -18,6 +18,9 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Instagram : sir.n3wt0n_")
 
+async def hayatanjing(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("hayat anjing")
+
 async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Instagram : sir.n3wt0n_")
 
@@ -26,5 +29,6 @@ app = ApplicationBuilder().token("6320416802:AAEFPX1kePXFNt8o-ohSvtzINdsFaNrpOps
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help))
 app.add_handler(CommandHandler("contact", contact))
+app.add_handler(CommandHandler("hayatanjing", hayatanjing))
 
 app.run_polling()
